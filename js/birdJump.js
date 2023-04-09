d82d856f5f8ed9d4b4d1a5184f8d75e8e2965b76
Jakub Gaj <gaju2001@o2.pl>
2023-04-09 19:25:13 +0200

    fix bug

--- js/birdJump.js
+++ js/birdJump.js
@@ -1,19 +1,20 @@
 let keys = [];
 const findKey = (e) => {
-  let jumpSound = new Audio("audio/cartoon-jump-6462.mp3");
-  jumpSound.volume = 0.5;
-  jumpSound.play();
-  let backgroundMusic = document.querySelector(".music");
-  backgroundMusic.play();
-  document.querySelector(".bird__jump-clouds").style.display = "block";
-  setTimeout(() => {
-    document.querySelector(".bird__jump-clouds").style.display = "none";
-  }, 500);
-  document.querySelector(".instruction").style.display = "none";
   if (e.key === "w") {
     keys.push(e.key);
   }
   if (keys.length === 1) {
+    let jumpSound = new Audio("audio/cartoon-jump-6462.mp3");
+    jumpSound.volume = 0.5;
+    jumpSound.play();
+    let backgroundMusic = document.querySelector(".music");
+    backgroundMusic.play();
+    document.querySelector(".bird__jump-clouds").style.display = "block";
+    setTimeout(() => {
+      document.querySelector(".bird__jump-clouds").style.display = "none";
+    }, 500);
+    document.querySelector(".instruction").style.display = "none";
+
     const jump = () => {
       let birdY = bird.getBoundingClientRect().y;
       bird.style.top = birdY - 4 + "px";
